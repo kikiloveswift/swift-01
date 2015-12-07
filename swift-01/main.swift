9f612302994fd50f6860cccf6894dff7cf6c1d27
Young <konglee1@outlook.com>
2015-12-07 20:48:58 +0800

    submit

--- swift-01/main.swift
+++ swift-01/main.swift
@@ -8,5 +8,13 @@
 
 import Foundation
 
-print("Hello, World!")
-
+var array = [1,2,3]
+var stringArray = ["jack","rose"] //可以这样写，如果不确定装入数据类型可以这样写
+var mixArrray = [1,"Tom"] //数据混装
+var strictArray : [String] = ["jack", "rose","strictArray"]//严格数据
+print(array)
+print(stringArray)
+print(stringArray.last)
+print(stringArray.first)
+print(mixArrray)
+print(strictArray)
